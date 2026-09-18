f6b662f64d42d694166f1e112862495957c8ce1c
bobducspng <bobducs@hotmail.com>
2026-09-18 16:50:04 -0300

    feat: adicionar perfil super administrador e migração de roles

---
+++ drizzle/0004_closed_bedlam.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `allowed_users` MODIFY COLUMN `role` enum('user','admin','super_admin') NOT NULL DEFAULT 'user';--> statement-breakpoint
+ALTER TABLE `users` MODIFY COLUMN `role` enum('user','admin','super_admin') NOT NULL DEFAULT 'user';
